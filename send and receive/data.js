b8bd89f61837d5ac1a7253f4440480e404d9bdad
pratama404 <65237429+pratama404@users.noreply.github.com>
2022-10-16 07:58:24 +0700

    Update data.js

--- send and receive/data.js
+++ send and receive/data.js
@@ -35,16 +35,74 @@ const options = {
 client.on('message', function(topic, payload){
     if(topic == "nusabot/hujan"){
         document.getElementById("hujan").innerHTML = payload;
+        addData(chartHujan, payload.toString());
     }
     if(topic == "nusabot/suhu"){
         document.getElementById("suhu").innerHTML = payload;
+        addData(chartSuhu, payload.toString());
     }
     if(topic == "nusabot/kelembaban"){
         document.getElementById("kelembaban").innerHTML = payload;
+        addData(chartKelembaban, payload.toString());
     }
 })
 
 function publish(){
     data = document.getElementById("publish").value;
     client.publish('nusabot/data', data.toString(),{qos: 1, retain:true});
+}
+
+const cth = document.getElementById('chartHujan');
+const cts = document.getElementById('chartSuhu');
+const ctk = document.getElementById('chartKelembaban');
+
+const chartHujan = new Chart(cth, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Hujan',
+            data: [],
+            backgroundColor: 'rgba(200, 150, 70, 1)',
+            borderWidth: 1
+        }]
+    },
+});
+
+const chartSuhu = new Chart(cts, {
+  type: 'line',
+  data: {
+      labels: [],
+      datasets: [{
+          label: 'Suhu',
+          data: [],
+          backgroundColor: 'rgba(0, 0, 255, 1)',
+          borderWidth: 1
+      }]
+  },
+});
+
+const chartKelembaban = new Chart(ctk, {
+  type: 'line',
+  data: {
+      labels: [],
+      datasets: [{
+          label: 'Kelembaban',
+          data: [],
+          backgroundColor: 'rgba(0, 255, 0, 1)',
+          borderWidth: 1
+      }]
+  },
+});
+
+
+function addData(chart, data) {
+  let currentDate = new Date();
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+  chart.data.labels.push(time);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+  });
+  chart.update();
 }
